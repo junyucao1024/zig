518e0f0bff4619867a5bf9620c5e516354b7496b
Jacob Young <jacobly0@users.noreply.github.com>
2025-04-21 23:28:33 -0400

    cbe: port to new `std.io.BufferedWriter` API

--- src/Zcu.zig
+++ src/Zcu.zig
@@ -2682,7 +2682,7 @@ pub fn loadZirCacheBody(gpa: Allocator, header: Zir.Header, cache_file: std.fs.F
     var cache_fr = cache_file.reader();
     var cache_br = cache_fr.interface().unbuffered();
     cache_br.readVecAll(&vecs) catch |err| switch (err) {
-        error.ReadFailed => if (cache_fr.err) |_| unreachable else |e| return e,
+        error.ReadFailed => return cache_fr.err.?,
         error.EndOfStream => return error.UnexpectedFileSize,
     };
     if (data_has_safety_tag) {
@@ -2737,7 +2737,7 @@ pub fn saveZirCache(gpa: Allocator, cache_file: std.fs.File, stat: std.fs.File.S
     var cache_fw = cache_file.writer();
     var cache_bw = cache_fw.interface().unbuffered();
     cache_bw.writeVecAll(&vecs) catch |err| switch (err) {
-        error.WriteFailed => if (cache_fw.err) |_| unreachable else |e| return e,
+        error.WriteFailed => return cache_fw.err.?,
     };
 }
 
@@ -2768,7 +2768,7 @@ pub fn saveZoirCache(cache_file: std.fs.File, stat: std.fs.File.Stat, zoir: Zoir
     var cache_fw = cache_file.writer();
     var cache_bw = cache_fw.interface().unbuffered();
     cache_bw.writeVecAll(&vecs) catch |err| switch (err) {
-        error.WriteFailed => if (cache_fw.err) |_| unreachable else |e| return e,
+        error.WriteFailed => return cache_fw.err.?,
     };
 }
 
@@ -2811,7 +2811,7 @@ pub fn loadZoirCacheBody(gpa: Allocator, header: Zoir.Header, cache_file: std.fs
     var cache_fr = cache_file.reader();
     var cache_br = cache_fr.interface().unbuffered();
     cache_br.readVecAll(&vecs) catch |err| switch (err) {
-        error.ReadFailed => if (cache_fr.err) |_| unreachable else |e| return e,
+        error.ReadFailed => return cache_fr.err.?,
         error.EndOfStream => return error.UnexpectedFileSize,
     };
     return zoir;
